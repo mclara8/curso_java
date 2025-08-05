844e8ff9fbd1cfafc696e29100bc816fc675e17d
mclara8 <mariiaclara428@gmail.com>
2025-08-04 22:06:57 -0300

    first commit

---
+++ src/estruturaSequencial/Soma.java
@@ -0,0 +1,22 @@
+package estruturaSequencial;
+import java.util.Scanner;
+
+public class Soma {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+       
+        int x, y, soma;
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        soma = x + y;
+
+        System.out.println("SOMA = " + soma);
+
+        sc.close();
+    }
+    
+}
