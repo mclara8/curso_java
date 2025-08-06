47823c7fe0366174cf7205d7d4134a6a6f196e91
mclara8 <mariiaclara428@gmail.com>
2025-08-05 22:21:46 -0300

    calculo de salario por horas trabalhadas

---
+++ src/estruturaSequencial/SalarioFuncionario.java
@@ -0,0 +1,31 @@
+package estruturaSequencial;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class SalarioFuncionario {
+    public static void main(String[] args) {
+    
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numeroFuncionario, horasTrabalhadas;
+        double valorPorHora, salarioFuncionario;
+
+        numeroFuncionario = sc.nextInt();
+        horasTrabalhadas = sc.nextInt();
+        valorPorHora = sc.nextDouble();
+
+        salarioFuncionario = horasTrabalhadas * valorPorHora;
+        
+        System.out.println("NUMBER = " + numeroFuncionario);
+        System.out.printf("SALARY =  U$ %.2f%n", salarioFuncionario);
+
+        sc.close();
+    }
+
+    
+
+    
+     
+}
