d0025478fa58c46154802d9dd9e24eeef133ef52
mclara8 <mariiaclara428@gmail.com>
2025-09-03 21:30:34 -0300

    inteiro impar ou par

---
+++ src/estuturaCondicional/ParOuImpar.java
@@ -0,0 +1,18 @@
+package estuturaCondicional;
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("PAR");
+        } else {
+            System.out.println("ÍMPAR");
+        }
+
+        sc.close();
+    }
+}
