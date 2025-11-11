f3d476de686de1242a04cd5e8af0b6bd73146a16
mclara8 <mariiaclara428@gmail.com>
2025-11-10 21:26:40 -0300

    exercicio 1 de estrutura repetitiva

---
+++ src/estruturaRepetitiva/ContagemImpares.java
@@ -0,0 +1,19 @@
+package estruturaRepetitiva;
+import java.util.Scanner;
+
+public class ContagemImpares {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            if (i % 2 != 0){
+                System.out.println(i);
+            } 
+        }
+
+        sc.close();
+    }
+}
